c7ce98a636666309e8f352191b0ec8148b26f6d0
Yazid <Alabaster456@users.noreply.github.com>
2014-04-12 21:13:36 -0400

    Update Piratescript.js

--- Piratescript.js
+++ Piratescript.js
@@ -1,1 +1,8 @@
 //this is for the javascript code
+
+//DO NOT GET THIS MIXED UP WITH JAVASCRIPT CODE. THIS IS JQUERY!
+$(document).ready(function(){
+$('#firstpart').fadeIn('slow');
+  $('#next').delay(1000).fadeIn('slow');
+});
+//END
